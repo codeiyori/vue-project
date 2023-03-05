31949b77a2cbe8ea6f67d9c6ea74fcff7da4511a
codeiyori <itnecommerce@gmail.com>
2023-03-04 19:26:08 -0500

    3차 수정

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   title 수정1
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/ted">ted</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ src/views/tedPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="ted">
+    <h1>This is an ted page</h1>
+  </div>
+</template>
